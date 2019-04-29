bb1790d9f093b3edf944717851628b0caae931ef
serg <serg@sergs-iMac.local>
2019-04-29 20:29:48 +0300

    remove unnecessary functions from libft

--- libft/libft.h
+++ libft/libft.h
@@ -21,40 +21,24 @@
 
 # include <stddef.h>
 
+/*
+**	memory functions
+*/
+
 void		*ft_memcpy(void *dst, const void *src, size_t n);
-
-size_t		ft_strlen(const char *s);
-
-char		*ft_strdup(const char *s1);
-char		*ft_strcpy(char *dst, const char *src);
-char		*ft_strncpy(char *dst, const char *src, size_t len);
-
-char		*ft_strcat(char *s1, const char *s2);
-char		*ft_strncat(char *s1, const char *s2, size_t n);
-
-size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
-
-char		*ft_strchr(const char *s, int c);
-char		*ft_strrchr(const char *s, int c);
-char		*ft_strstr(const char *haystack, const char *needle);
-char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
-
-int			ft_strcmp(const char *s1, const char *s2);
-int			ft_strncmp(const char *s1, const char *s2, size_t n);
+void		*ft_malloc_or_die(size_t size);
 
 /*
 **	string functions
 */
 
+size_t		ft_strlen(const char *s);
+int			ft_strcmp(const char *s1, const char *s2);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strdup(const char *s1);
 char		*ft_strsub(char const *s, unsigned int start, size_t len);
-char		*ft_strjoin(char const *s1, char const *s2);
 
 char		**ft_strsplit(char const *s, char c);
-
-/*
-**	generic table function
-*/
-
 void		ft_clear_tab(void **tab);
 
 /*
